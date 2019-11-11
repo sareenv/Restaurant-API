b2e35748e3473afbc75442a223aab458f59ecda7
Vinayak <sareenv@uni.coventry.ac.uk>
2019-11-11 18:39:30 +0000

    bug fixes

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ const staticDir = require('koa-static')
 const orderRouter = require('./routes/order-routes')
 const orderViewRouter = require('./routes/order-views-router')
 
-const defaultPort = 8080
+const defaultPort = 7890
 const port = process.env.PORT || defaultPort
 
 const views = require('koa-views')
 
 app.use(views(`${__dirname}/views`, { extension: 'handlebars' }, {map: { handlebars: 'handlebars' }}))
 app.use(staticDir('public'))
-
 
 app.use(orderViewRouter.routes())
 app.use(orderRouter.routes())
--- modules/order.js
+++ modules/order.js
@@ -3,15 +3,16 @@
 class Order {
 	constructor(database) {
 		this.database = database
+		this.collection = database.collection('Orders')
 	}
 
 	async orderRegistration(tablenumber, orderedItems) {
 		if (tablenumber === undefined) throw new Error('missing table number')
 		if (orderedItems === undefined) throw new Error('missing orderedItems')
-		if(isNaN(parseInt(tablenumber))) throw new Error('Invalid type for table number')
+		if(isNaN(parseInt(tablenumber))) throw new Error('Invalid table number')
 		try {
-			// eslint-disable-next-line max-len
-			await this.database.collection('Orders').insertOne({tablenumber: tablenumber, orderedItems: orderedItems, pending: true})
+			const details = {tablenumber: tablenumber, orderedItems: orderedItems, pending: true}
+			await this.collection('Orders').insertOne(details)
 			return true
 		}catch(error) {
 			throw error
