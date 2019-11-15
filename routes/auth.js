622784f446b213925944a25064cfbfc640937b3c
Vinayak <sareenv@uni.coventry.ac.uk>
2019-11-15 09:51:45 +0000

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,43 +1,41 @@
 'use strict'
 
 const Router = require('koa-router')
-const koaBody = require('koa-body')({multipart: true, uploadDir: '.'})
+const jwt = require('jsonwebtoken')
+const bodyParser = require('koa-bodyparser')
 const router = new Router()
 
 const Staff = require('../modules/staff')
 const authDb = require('../databases/authDb')
 
+const unauthorisedStatusCode = 401
 
-router.post('/register', koaBody, async ctx => {
+router.post('/register', bodyParser(), async ctx => {
 	const {username, password, name, memberType} = ctx.request.body
 	const staff = new Staff(authDb.database)
 	try{
 		await staff.registration(username, password, name, memberType)
-		ctx.redirect('/login')
+		return ctx.body = true
 	}catch(error) {
 		await ctx.render('register', {error: error})
 	}
 })
 
-router.post('/login', koaBody, async ctx => {
+router.post('/login', bodyParser(), async ctx => {
 	const {username, password} = ctx.request.body
 	const staff = new Staff(authDb.database)
 	try{
 		const authResult = await staff.login(username, password)
 		if(authResult === false) {
-			return await ctx.render('login', {error: 'Authentication Failed! Wrong Credentials'})
+			return ctx.throw(unauthorisedStatusCode, {error: 'Details are not found in our system'})
 		}
-
-		ctx.session = {authorised: true, staff: username}
-		ctx.redirect('/welcome')
+		const staffType = await staff.memberType(username)
+		const token = await jwt.sign({username: username, memberType: staffType}, 'darkSecretPrivateKey340CT')
+		ctx.body = { token }
 	}catch(error) {
-		await ctx.render('login', {error: error})
+		console.log(error)
+		ctx.throw(unauthorisedStatusCode, {error: error})
 	}
-})
-
-router.get('/logout', async ctx => {
-	ctx.session.authorised = null
-	ctx.redirect('/login')
 })
 
 module.exports = router
