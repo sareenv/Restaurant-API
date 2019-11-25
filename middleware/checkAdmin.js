28431e8628a2ddca8a60102e1507d6b887d1077d
Vinayak <sareenv@uni.coventry.ac.uk>
2019-11-25 02:23:18 +0000

    changes

--- middleware/checkAdmin.js
+++ middleware/checkAdmin.js
@@ -1,7 +1,23 @@
 
 'use strict'
 
-function checkAdminAccount(ctx, next) {
+const jwt = require('jsonwebtoken')
+const unauthorisedStatusCode = 401
+
+async function checkAdminAccount(ctx, next) {
+	const authHeader = ctx.headers.authorization
+	const authMessage = 'Authorisation Header not found, Cannot verify you'
+	if(authHeader === undefined) {
+		ctx.status = unauthorisedStatusCode
+		return ctx.body = {error: true, message: authMessage}
+	}
+	const jwtToken = authHeader
+	const verify = await jwt.verify(jwtToken, 'darkSecret340Admin')
+	const memberType = verify.memberType
+	if(memberType !== 'admin') {
+		ctx.status = unauthorisedStatusCode
+		return ctx.body = {error: true, message: 'Only kitchen staff can access this resource'}
+	}
 	return next()
 }
 
--- routes/adminLogin.js
+++ routes/adminLogin.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 const jwt = require('jsonwebtoken')
 
 const router = new Router()
@@ -8,14 +9,20 @@ const unauthorisedStatusCode = 401
 const Admin = require('../modules/admin')
 const adminDb = require('../databases/adminDb')
 
-router.post('/adminLogin', async ctx => {
+router.post('/adminLogin', bodyParser(), async ctx => {
 	const {username, password} = ctx.request.body
 	const admin = new Admin(adminDb.database)
-	const result = admin.adminLogin(username, password)
-	const token = await jwt.sign({username: username, memberType: admin}, 'darkSecretPrivateKey340CTAdmin')
-	if(result === true) return ctx.body = {error: false, token: token}
-	ctx.response.status = unauthorisedStatusCode
-	return ctx.body = {error: true, message: 'Error validating credentials'}
+	try{
+		const result = await admin.adminLogin(username, password)
+		const secret = 'darkSecret340Admin'
+		const token = await jwt.sign({ exp: 1800, data: {username: username, memberType: 'admin'} }, secret)
+		if(result === true) return ctx.body = {error: false, token: token}
+		ctx.response.status = unauthorisedStatusCode
+		return ctx.body = {error: true, message: 'Error validating credentials'}
+	}catch(error) {
+		ctx.response.status = unauthorisedStatusCode
+		return ctx.body = {error: true, message: 'Error validating credentials'}
+	}
 })
 
 module.exports = router
