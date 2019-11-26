f257ec54d75d4bf74f612e6fb1453ae4240e0f0c
Vinayak <sareenv@uni.coventry.ac.uk>
2019-11-26 01:23:07 +0000

    script changes

--- start.sh
+++ start.sh
@@ -5,10 +5,13 @@ sudo lsof -ti:9090 | xargs kill
 sudo lsof -ti:8888 | xargs kill
 sudo lsof -ti:8989 | xargs kill
 echo 'Stopped services accessing the ports these microservice require'
-node service/admin.js &
-node service/authentication.js &
-node service/order.js &
+sudo node service/admin.js &
+sudo node service/authentication.js &
+sudo node service/order.js &
 echo '--------------------------------'
 echo 'Microservices are running in background mode.'
 read -p "Press enter to continue"
+sudo lsof -ti:9090 | xargs kill
+sudo lsof -ti:8888 | xargs kill
+sudo lsof -ti:8989 | xargs kill
 kill %1
