db2d9219bb3f3dd7b8d232abcfb40c3d8ced1121
vinayak <sareenv@uni.coventry.ac.uk>
2019-11-30 17:38:12 +0000

    changes

--- __mocks__/fs-extra.js
+++ __mocks__/fs-extra.js
@@ -4,7 +4,6 @@ function writeFile(fileName, data) {
 	const filedata = {name: fileName, content: data}
 	const contents = [{name: 'hello.txt', content: 'profit'}]
 	const compareResults = fileName === 'hello.txt' && data === 'profit'
-	console.log(fileName)
 	if(compareResults) return Promise.reject(false)
 	contents.push(filedata)
 	return Promise.resolve(true)
